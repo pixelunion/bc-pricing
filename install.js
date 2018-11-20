7ed403dfc5cec7270cc259f8b03dd6b34f3e4018
maenda <maenda@pixelunion.net>
2018-11-20 11:40:57 -0500

    Update install.js

--- install.js
+++ install.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 
 console.log('something cool');
 
-fs.copyFile('test.txt', 'destination.txt', (err) => {
+fs.copyFile('test.txt', '../../destination.txt', (err) => {
   if (err) throw err;
   console.log('test.txt was copied to destination.txt');
 });
