0d2f80061f1c27558b7994678044b5e149f42c44
Maenda Fortier <maenda@pixelunion.net>
2018-11-21 15:39:10 -0500

    m

--- install.js
+++ install.js
@@ -4,8 +4,6 @@ const path = require('path');
 
 const themePath = '../../../';
 const modulePath = process.cwd();
-console.log(path.resolve(__dirname, 'test.txt'));
-console.log(path.resolve(themePath, 'destination.txt'));
 
 fs.copyFile(path.resolve(__dirname, 'test.txt'), path.resolve(themePath, 'destination.txt'), (err) => {
   if (err) throw err;
--- install.sh
+++ install.sh
@@ -1,1 +1,10 @@
 echo 'Installing'
+# sync() {
+#   echo
+#   echo "----- Installing BC Pricing"
+#   echo
+#   rsync -v -h -r -i --delete "$2" "$3"
+# }
+
+# sync "Templates" "$DIR/templates/core" "../../../templates/"
+echo $DIR
