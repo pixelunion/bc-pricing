84d1722fb79fa19054672d38801f65ac4b3e53df
Maenda Fortier <maenda@pixelunion.net>
2018-11-21 15:06:18 -0500

    m

--- install.js
+++ install.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 
 const themeFolder = '../../';
 
-console.log(process);
+console.log(process.argv);
 fs.copyFile('test.txt', themeFolder + 'destination.txt', (err) => {
   if (err) throw err;
   console.log('test.txt was copied to destination.txt');
