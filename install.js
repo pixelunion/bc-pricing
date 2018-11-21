7ae0130e8d943c90570fd1ab11ed4e222fdc185e
Maenda Fortier <maenda@pixelunion.net>
2018-11-21 15:27:12 -0500

    m

--- install.js
+++ install.js
@@ -5,7 +5,7 @@ const path = require('path');
 const themePath = '../../';
 const modulePath = process.cwd();
 
-fs.copyFile('test.txt', path.resolve(themePath, 'destination.txt'), (err) => {
+fs.copyFile(path.resolve(__dirname, 'test.txt'), path.resolve(themePath, 'destination.txt'), (err) => {
   if (err) throw err;
   console.log('test.txt was copied to destination.txt');
 });
