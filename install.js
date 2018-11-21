dc3b0885a20d4275b8783378ecc79687d39353cd
Maenda Fortier <maenda@pixelunion.net>
2018-11-21 15:30:03 -0500

    m

--- install.js
+++ install.js
@@ -4,7 +4,9 @@ const path = require('path');
 
 const themePath = '../../';
 const modulePath = process.cwd();
-console.log(themePath);
+console.log(path.resolve(__dirname, 'test.txt'));
+console.log(path.resolve(themePath, 'destination.txt'));
+
 fs.copyFile(path.resolve(__dirname, 'test.txt'), path.resolve(themePath, 'destination.txt'), (err) => {
   if (err) throw err;
   console.log('test.txt was copied to destination.txt');
