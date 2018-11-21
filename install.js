6c205e23536fa9971abbc0678c805f2d6e2db234
Maenda Fortier <maenda@pixelunion.net>
2018-11-21 15:23:30 -0500

    m

--- install.js
+++ install.js
@@ -1,11 +1,12 @@
-global.__basedir = __dirname;
 
 const fs = require('fs');
+const path = require('path');
 
 const themePath = '../../';
 const modulePath = process.cwd();
+
 console.log(__basedir);
-fs.copyFile('test.txt', themePath + 'destination.txt', (err) => {
+fs.copyFile('test.txt', path.resolve(themePath, 'destination.txt'), (err) => {
   if (err) throw err;
   console.log('test.txt was copied to destination.txt');
 });
