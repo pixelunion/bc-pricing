6a78e1deb1fd5a8d878b0b3aa53e8fb828a3dd18
Maenda Fortier <maenda@pixelunion.net>
2018-11-21 16:08:33 -0500

    m

--- install.js
+++ install.js
@@ -6,7 +6,12 @@ var ncp = require('ncp').ncp;
 const themePath = '../../../';
 const modulePath = process.cwd();
 
-ncp(path.resolve(themePath, 'templates'), path.resolve(modulePath, 'templates'));
+ncp(path.resolve(themePath, 'templates'), path.resolve(modulePath, 'templates'), function(err) {
+  if (err) {
+    return console.error(err);
+  }
+  console.log('done!');
+});
 // fs.copyFile(path.resolve(__dirname, 'test.txt'), path.resolve(themePath, 'destination.txt'), (err) => {
 //   if (err) throw err;
 //   console.log('test.txt was copied to destination.txt');
