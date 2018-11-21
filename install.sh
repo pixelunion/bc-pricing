c821989b347a40afc5444756c7bdafee4e2a678b
Maenda Fortier <maenda@pixelunion.net>
2018-11-21 15:52:42 -0500

    m

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 echo 'Installing'
-# sync() {
-#   echo
-#   echo "----- Installing BC Pricing"
-#   echo
-#   rsync -v -h -r -i --delete "$2" "$3"
-# }
+echo dirname $3
+sync() {
+  echo
+  echo "----- Installing BC Pricing"
+  echo
+  rsync -v -h -r -i --delete "$2" "$3"
+}
 
 sync "Templates" "$DIR/templates/pricing" "../../../templates/"
-echo dirname $3
